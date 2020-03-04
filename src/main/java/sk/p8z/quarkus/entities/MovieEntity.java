366e6766ee2b1b0703ef9dfb87ab37bb87bffb37
zabkap <Pavol.ZABKA@posam.sk>
2020-03-04 11:15:44 +0100

    added MovieEntity

---
+++ src/main/java/sk/p8z/quarkus/entities/MovieEntity.java
@@ -0,0 +1,23 @@
+package sk.p8z.quarkus.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "movies", schema = "public", catalog = "docker")
+public class MovieEntity {
+    @Id
+    @Column(nullable = false)
+    private Integer id;
+    @Column
+    private String title;
+    @Column
+    private Integer year;
+    @Column
+    private Integer length;
+    @Column
+    private String genres;
+    private String lowercaseTitle;
+}
