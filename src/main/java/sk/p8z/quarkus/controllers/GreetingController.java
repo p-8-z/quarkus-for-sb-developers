1dbf0568a7aba4db54f1d7b16aa5b20583a9bbba
zabkap <Pavol.ZABKA@posam.sk>
2020-03-04 11:17:50 +0100

    removed PathVariable from GreetingController

--- src/main/java/sk/p8z/quarkus/controllers/GreetingController.java
+++ src/main/java/sk/p8z/quarkus/controllers/GreetingController.java
@@ -3,7 +3,6 @@ package sk.p8z.quarkus.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
